34072a7e67fb900c289b293c1f92a5ab58764057
Mugami <aguvasumugami@gmail.com>
2025-07-10 18:29:13 +0300

    new email addition

--- main_2.py
+++ main_2.py
@@ -10,11 +10,12 @@ from nigeria_ebid import ng_ebid_scrapper
 from nigeria_etenders import ng_etenders_scrapper
 from nigeria_tenders import ng_procurement_scrapper
 from ppip import ppip_scraper
+from rwanda_ucwa import scrape_rwanda_data
 from tz_pprea import tz_scrapper
 from uganda_tenders import ug_scraper
 from utils import format_results_as_html, send_email
 from world_bank import wb_scrape
-from rwanda_ucwa import scrape_rwanda_data
+
 load_dotenv()
 
 error_email_1 = os.environ.get("ERROR_EMAIL_1")
@@ -23,8 +24,9 @@ email_2 = os.environ.get("RECEIPT_EMAIL_2")
 email_3 = os.environ.get("RECEIPT_EMAIL_3")
 email_4 = os.environ.get("RECEIPT_EMAIL_4")
 email_5 = os.environ.get("RECEIPT_EMAIL_5")
+email_6 = os.environ.get("RECEIPT_EMAIL_6")
 
-all = [email_5, email_4, email_2, email_3, email_1]
+all = [email_5, email_4, email_2, email_3, email_1, email_6]
 logging.basicConfig(
     filename="scraper_errors.log",
     level=logging.ERROR,
@@ -40,12 +42,12 @@ def main():
         "Ethiopia Tenders": egp_scraper,
         "TZ Tenders": tz_scrapper,
         "PPIP Tenders": ppip_scraper,
-        #"Uganda Tenders": ug_scraper,
+        "Uganda Tenders": ug_scraper,
         "World Bank Tenders": wb_scrape,
         "Rwanda Tenders": scrape_rwanda_data,
-        "Nigeria e-Bid":ng_ebid_scrapper,
-        "Nigeria e-Tenders":ng_etenders_scrapper,
-        "Nigeria Procurement":ng_procurement_scrapper
+        "Nigeria e-Bid": ng_ebid_scrapper,
+        "Nigeria e-Tenders": ng_etenders_scrapper,
+        "Nigeria Procurement": ng_procurement_scrapper
     }
     results = {}
     with ThreadPoolExecutor() as executor:
@@ -66,7 +68,7 @@ def main():
 
     if errors:
         send_email(
-            subject="Tender Scraper Errors", 
+            subject="Tender Scraper Errors",
             body=f"<h3>The following errors occurred during scraping:</h3><p>{'<br>'.join(errors)}</p>",
             recipients=[error_email_1]
         )
